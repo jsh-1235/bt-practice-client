06cc6b1e546045bb2e0d4156bc810c397a101978
jsh-1235 <jsh-1235@hanmail.net>
2024-09-25 13:00:25 +0900

    initialize

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <p>BT Practice Client</p>
+        <a className="App-link" href="http://www.btinc.co.kr" target="_blank" rel="noopener noreferrer">
+          http://www.btinc.co.kr
         </a>
       </header>
     </div>
